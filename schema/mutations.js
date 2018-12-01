ee2be2f35dcb6614e4bb9068288dc4fca70bc798
Matt Zuzolo <mattzuzolo@outlook.com>
2018-12-01 15:22:57 -0500

    Removed commented code and unneccessary console logs from mutation.js

--- schema/mutations.js
+++ schema/mutations.js
@@ -23,25 +23,9 @@ const mutation = new GraphQLObjectType({
         let user = new User(args);
         console.log("NEW USER INSTANCE TO SAVE", user);
         user.save()
-          .then(console.log("SUCCESS"))
-          .catch(error => console.log("FAILURE!", error));
       }
     },
   }
 })
-
-// function configureAddUser(args){
-//   console.log("Configuring POST request...")
-//   let config = {
-//     Accept: "application/json",
-//     method: "POST",
-//     headers: {
-//       "Content-type": "application/json"
-//     },
-//     body: JSON.stringify(args)
-//   };
-//   console.log("CONFIG: ", config);
-//   return config;
-// }
 
 module.exports = mutation;

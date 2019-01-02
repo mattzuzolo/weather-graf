5474e51ec04329f527960b77d6d2608bd587d32b
Matt Zuzolo <mattzuzolo@outlook.com>
2019-01-02 15:17:32 -0500

    Built WeatherCard class to generate cards in front end

---
+++ public/js/WeatherCard.js
@@ -0,0 +1,13 @@
+class WeatherCard {
+  constructor(id, name, latitude, longitude){
+    this.id = id;
+    this.name = name;
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  buildCard(){
+    console.log("Building card...")
+  }
+
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,7 @@
 console.log("JS is connected");
+
+let testCard = new WeatherCard(123, "Lower East Side", "-45.53", "34.56");
+
+console.log("testCard", testCard);
+
+testCard.buildCard();
